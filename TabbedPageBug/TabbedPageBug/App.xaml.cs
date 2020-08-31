6bf51fa3743fda4edc325540fadeca90800b912f
BaseCrusher <ruzinski.jehuda@outlook.com>
2020-08-31 20:56:29 +0200

    added project

---
+++ TabbedPageBug/TabbedPageBug/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedPageBug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            PageAppearing += (o, args) => 
+            {
+                Debug.WriteLine($"Page {args.Title}");
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
